e142a8b546b5c247ad04ef71887ba082d3247e7e
dell3560 <dell3560@DESKTOP-OPQ6431>
2026-08-15 14:52:55 +0300

    Questione 1

--- c#Assegment 1/Program.cs
+++ c#Assegment 1/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace c_Assegment_1
+﻿namespace Assignment1
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            
-        }
+ class Book
+{
+//        // #region Question 1
+//        //Create a Book class with a Title(string) and Pages(int). Create a Book object and store
+//        //    it in a variable of type object.Print it.
+
+//string Title = "c# Programming";
+//int pages = 500;
+//        static void Main()
+//        {
+//            Book book = new Book();
+//             Console.WriteLine($"Title: {book.Title}, Pages: {book.pages}");
+//        }
+//          #endregion
     }
 }
